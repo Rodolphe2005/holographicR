c6bfc6f389a0422d35f8e604d6f6cfdfbd1dcc62
Rodolphe LAMPE <rodolphe.lampe@external.engie.com>
2016-09-21 17:26:32 +0200

    First version working :)

--- HRS.R
+++ HRS.R
@@ -1,8 +1,11 @@
 library(ggplot2)
 library(viridis)
 library(dplyr)
+library(gridExtra)
 data(diamonds)
 
+# Transform a factor or numeric vector x to an integer vector taking values in
+# 0, ..., n-1
 transform_to_integer <- function(x, n) {
     if (is.factor(x)){
         if(nlevels(x) != n)
@@ -18,90 +21,108 @@ transform_to_integer <- function(x, n) {
     
 }
 
+# This function creates a plot which represents a function taking n different
+# values and increases/decreases waves times. 
+# For example, for a plot that increases and then decreases, waves = 2
+create_waves <- function(n, waves = 1, color = "firebrick1", location_top = T){
+    increasing_wave <- expand.grid(x = 1:n, y = 1:n) %>% mutate(fill = (y == x))
+    decreasing_wave <- expand.grid(x = 1:n, y = 1:n) %>% mutate(fill = (y == n+1-x))
+    
+    num_increasing_waves <- floor((waves+1)/2)
+    num_decreasing_waves <- floor(waves/2)
+    
+    increasing_waves <- 1:num_increasing_waves %>% 
+        lapply(function(k) increasing_wave %>% mutate(x = x + 2*n*(k-1))) %>% 
+        bind_rows()
+    
+    if (num_decreasing_waves>0){
+        decreasing_waves <- 1:num_decreasing_waves %>% 
+            lapply(function(k) decreasing_wave %>% mutate(x = x + n + 2*n*(k-1))) %>% 
+            bind_rows()
+    } else {
+        decreasing_waves <- NULL
+    }
+    
+    df <- rbind(increasing_waves, decreasing_waves)
+    if(location_top){
+        g <- ggplot(df, aes(x, y, fill = fill)) +
+             scale_x_continuous(expand = c(0,0))
+    } else {
+        g <- ggplot(df, aes(y, x, fill = fill)) +
+            scale_x_continuous(expand = c(0,0), trans = "reverse")
+    }
+
+    g + geom_tile() + 
+        scale_fill_manual(values = c("white", color)) +
+        scale_y_continuous(expand = c(0,0)) +
+        theme(axis.text = element_blank(),
+              axis.title = element_blank(),
+              axis.ticks = element_blank(),
+              panel.grid = element_blank(),
+              legend.position = "none",
+              panel.margin = unit(0, units = "cm"),
+              plot.margin = unit(c(0,0,0,0), units = "cm"))
+        
+}
+create_waves(7,5, "firebrick1", T)
+
+
+
+
+
+
 transform_to_integer(diamonds$cut, 5)
 transform_to_integer(diamonds$carat, 5)
 
 df <- diamonds
 var1 = "carat"
-n1 = 5
+n1 = 4
 var2 = "cut"
 n2 = 5
 target = "price"
 
+var5 = "clarity"
+n5 = 8
+var6 = "color"
+n6 = 7
 
 df2 <- data.frame(target = df[[target]])
-x1 <- transform_to_integer(df[[var1]], n1)
-x2 <- transform_to_integer(df[[var2]], n2)
-df2$x_axis <- n2*x1 + ifelse(x1%%2 == 0, x2, n2-1-x2)
-
-# Plot of the x axis
-my_theme <- theme(axis.text = element_blank(),
+df2$x1 <- transform_to_integer(df[[var1]], n1)
+df2$x2 <- transform_to_integer(df[[var2]], n2)
+df2$y1 <- transform_to_integer(df[[var5]], n5)
+df2$y2 <- transform_to_integer(df[[var6]], n6)
+main_g <- df2 %>% 
+    mutate(
+        x_axis = n2*x1 + ifelse(x1%%2 == 0, x2, n2-1-x2),
+        y_axis = n6*y1 + ifelse(y1%%2 == 0, y2, n6-1-y2)
+    ) %>% 
+    group_by(x_axis, y_axis) %>% 
+    summarise(target = mean(target)) %>% 
+    ggplot(aes(x = x_axis, y = y_axis, fill = target)) +
+    geom_tile() +
+    scale_fill_viridis() +
+    scale_y_continuous(expand = c(0,0)) +
+    scale_x_continuous(expand = c(0,0)) +
+    theme(axis.text = element_blank(),
           axis.title = element_blank(),
           axis.ticks = element_blank(),
           panel.grid = element_blank(),
           legend.position = "none",
           panel.margin = unit(0, units = "cm"),
           plot.margin = unit(c(0,0,0,0), units = "cm"))
-p1 <- data.frame(expand.grid(x = 1:n1, y = 1:n1)) %>%
-    mutate(fill = (x==y)) %>% 
-    ggplot(aes(x, y, fill = fill)) +
-    geom_tile() +
-    scale_fill_manual(values = c("white", "firebrick1")) +
-    scale_y_continuous(expand = c(0,0)) +
-    scale_x_continuous(expand = c(0,0)) +
-    my_theme
-
-p2 <- data.frame(expand.grid(x = 1:(n1*n2), y = 1:n2)) %>%
-    mutate(fill = ((x-1)%%n2==(y-1) & ((x-1)%/%n2)%%2 ==0) | 
-                  ((x-1)%%n2 == (n2-y) & (((x-1)%/%n2)%%2 == 1))) %>%
-    ggplot(aes(x,y,fill = fill)) +
-    geom_tile() +
-    scale_fill_manual(values = c("white", "dodgerblue")) +
-    scale_y_continuous(expand = c(0,0)) +
-    scale_x_continuous(expand = c(0,0)) +
-    my_theme
-
-p3 <- data.frame(expand.grid(x = 1:(n1*n2), y = 1:n2)) %>%
-    mutate(fill = ((x-1)%%n2==(y-1) & ((x-1)%/%n2)%%2 ==0) | 
-               ((x-1)%%n2 == (n2-y) & (((x-1)%/%n2)%%2 == 1))) %>%
-    ggplot(aes(x,y,fill = fill)) +
-    geom_tile() +
-    scale_fill_manual(values = c("white", "dodgerblue")) +
-    scale_y_continuous(expand = c(0,0)) +
-    scale_x_continuous(expand = c(0,0)) +
-    my_theme
-
-
 
 hlay <- rbind(c(NA,NA,1),
               c(NA,NA,2),
               c(3,4,5))
-grid.arrange(grobs=list(p1,p2,p1,p2,p3), 
+grid.arrange(grobs=list(create_waves(n2,n1, "firebrick1", T),
+                        create_waves(n1,1, "dodgerblue", T),
+                        create_waves(n6,n5, "black", F),
+                        create_waves(n5,1, "grey", F),
+                        main_g), 
              layout_matrix=hlay,
              heights = unit(c(1,1,12), c("cm")),
              widths = unit(c(1,1,18), c("cm")),
              padding = 0)
-
-
-
-
-
-
-
-
-
-diamonds$carat_cutted <- cut(diamonds$carat, 
-                             breaks = quantile(diamonds$carat), 
-                             labels = F, 
-                             include.lowest = T)
-diamonds$x_axis <- nlevels(diamonds$cut)*diamonds$carat_cutted + as.numeric(diamonds$cut)
-
-diamonds %>% 
-    group_by(x_axis, color) %>%
-    summarise(price = mean(price)) %>%
-    ggplot(aes(x = x_axis, y = color, fill = price)) +
-    geom_tile() +
-    scale_fill_viridis()
 
 
 
